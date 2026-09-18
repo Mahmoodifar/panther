c9e3dc49b3cc5f05d659c4b1c23e3d1195634f1e
sajad mahmoodifar <sajadmahmoodifar1995@gmail.com>
2026-09-18 13:24:59 +0330

    test: match existing test return types

--- tests/ProcessManager/WebServerReadinessProbeTraitTest.php
+++ tests/ProcessManager/WebServerReadinessProbeTraitTest.php
@@ -25,7 +25,7 @@ class WebServerReadinessProbeTraitTest extends TestCase
     /**
      * @dataProvider provideTerminatedProcesses
      */
-    public function testTerminatedProcess(bool $disableOutput, string $expectedMessage)
+    public function testTerminatedProcess(bool $disableOutput, string $expectedMessage): void
     {
         $process = new Process([\PHP_BINARY, '-r', 'fwrite(STDERR, "Startup failed."); exit(1);']);
         if ($disableOutput) {
